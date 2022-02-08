8a2470ad8abfc17f14599ffc4124bb5dc1b0c321
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2022-02-09 00:55:49 +0100

    style: add prettier integration to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,24 @@
 module.exports = {
-	env: {
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		'plugin:react/recommended',
-		'xo',
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react',
-		'@typescript-eslint',
-	],
-	rules: {
-	},
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'xo', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<StatusBar style="auto" />
-			<Text>🕰 Countdown App</Text>
-			<TextInput style={{borderWidth: 2}}></TextInput>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>🕰 Countdown App</Text>
+      <TextInput style={{ borderWidth: 2 }}></TextInput>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
