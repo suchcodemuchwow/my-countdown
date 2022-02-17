872ddab66106b08702626f82834890375fbad0d4
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2022-02-18 00:01:30 +0100

    babel and ts changes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-module.exports = function (api) {
+module.exports = (/** @type {{ cache: (arg0: boolean) => void; }} */ api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
