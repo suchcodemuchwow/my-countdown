dc652bbce7091a9b111021c79c1d16886488121d
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2022-02-18 03:41:39 +0100

    refactor Navigation component

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,54 +1,53 @@
 import React from 'react';
-import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  NavigationContainer,
+  NavigationContainerRefWithCurrent,
+  useNavigationContainerRef,
+} from '@react-navigation/native';
+import { createStackNavigator, StackNavigationOptions } from '@react-navigation/stack';
 import { Button, ButtonProps, Icon } from '@ui-kitten/components';
 import { HomeScreen, OccasionFormScreen } from './screens';
 
+const enum ROUTES {
+  HOME = 'HOME',
+  FORM = 'FORM',
+}
+
 export type RootStackParamList = {
-  Home: undefined;
-  CreateOccasion: undefined;
+  HOME: undefined;
+  FORM: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
 
+const rootNavigatorScreenOptions: StackNavigationOptions = {
+  headerTitleAlign: 'center',
+  headerShadowVisible: true,
+};
+
+const formScreenOptions: StackNavigationOptions = {
+  presentation: 'modal',
+  headerShown: false,
+};
+
+const homeScreenOptions = (navigationRef: NavigationContainerRefWithCurrent<RootStackParamList>) => ({
+  title: 'Home',
+  headerRight: () => (
+    <Button appearance="ghost" accessoryLeft={PlusIcon} onPress={() => navigationRef.navigate(ROUTES.FORM)} />
+  ),
+});
 const PlusIcon = (props?: ButtonProps) => <Icon {...props} name="plus" />;
 
 export const AppNavigator = () => {
   const navigationRef = useNavigationContainerRef<RootStackParamList>();
   return (
     <NavigationContainer ref={navigationRef}>
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleAlign: 'center',
-          headerShadowVisible: true,
-        }}
-      >
+      <Stack.Navigator initialRouteName={ROUTES.FORM} screenOptions={rootNavigatorScreenOptions}>
         <Stack.Group>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              title: 'Home',
-              headerRight: () => (
-                <Button
-                  style={{}}
-                  appearance="ghost"
-                  accessoryLeft={PlusIcon}
-                  onPress={() => navigationRef.navigate('CreateOccasion')}
-                />
-              ),
-            }}
-          />
+          <Stack.Screen name={ROUTES.HOME} component={HomeScreen} options={homeScreenOptions(navigationRef)} />
         </Stack.Group>
         <Stack.Group>
-          <Stack.Screen
-            name="CreateOccasion"
-            component={OccasionFormScreen}
-            options={{
-              presentation: 'modal',
-              headerShown: false,
-            }}
-          />
+          <Stack.Screen name={ROUTES.FORM} component={OccasionFormScreen} options={formScreenOptions} />
         </Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
